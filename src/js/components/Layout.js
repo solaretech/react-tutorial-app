a5d479370a2c10797f4737295623626f55ffa389
solaretech <solaretech.kk@gmail.com>
2020-08-13 16:16:05 +0900

    Add State

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -5,12 +5,16 @@ import Footer from "./Footer";
 export default class Layout extends React.Component {
   constructor(){
     super();
-    this.name = "Kakuny";
+    this.state = {name: "Kakuny"};
   };
   render() {
     let components = [<Header />, <Footer />];
+    setTimeout(
+      () => { this.setState({name: "Hello!"}); }
+    , 1000);
     return (
       <div>
+        {this.state.name}
         {components}
       </div>
     );
